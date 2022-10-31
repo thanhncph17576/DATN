7825ec0c23f1e68ba91b9f458768a7aaf0e03587
Nguyễn Trung Hiếu <93529612+Hieuhzzkk@users.noreply.github.com>
2022-10-31 23:26:17 +0700

    dll

--- src/main/java/poly/store/dao/OrderDetailDAO.java
+++
@@ -1,8 +0,0 @@
-package poly.store.dao;
-
-import org.springframework.data.jpa.repository.JpaRepository;
-
-import poly.store.entity.OrderDetail;
-
-public interface OrderDetailDAO extends JpaRepository<OrderDetail, Long>{
-}
