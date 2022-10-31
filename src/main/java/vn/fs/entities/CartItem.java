e770c937b45e637890abb83ac17de1ff8ff513ed
Nguyễn Trung Hiếu <93529612+Hieuhzzkk@users.noreply.github.com>
2022-10-31 23:26:53 +0700

    ncm

---
+++ src/main/java/vn/fs/entities/CartItem.java
@@ -0,0 +1,59 @@
+package vn.fs.entities;
+
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+/**
+ * @author DongTHD
+ *
+ */
+@Data
+@AllArgsConstructor
+@NoArgsConstructor
+public class CartItem {
+
+	private Long id;
+	private String name;
+	private double unitPrice;
+	private int quantity;
+	private double totalPrice;
+	private Product product;
+	public Long getId() {
+		return id;
+	}
+	public void setId(Long id) {
+		this.id = id;
+	}
+	public String getName() {
+		return name;
+	}
+	public void setName(String name) {
+		this.name = name;
+	}
+	public double getUnitPrice() {
+		return unitPrice;
+	}
+	public void setUnitPrice(double unitPrice) {
+		this.unitPrice = unitPrice;
+	}
+	public int getQuantity() {
+		return quantity;
+	}
+	public void setQuantity(int quantity) {
+		this.quantity = quantity;
+	}
+	public double getTotalPrice() {
+		return totalPrice;
+	}
+	public void setTotalPrice(double totalPrice) {
+		this.totalPrice = totalPrice;
+	}
+	public Product getProduct() {
+		return product;
+	}
+	public void setProduct(Product product) {
+		this.product = product;
+	}
+	
+}
